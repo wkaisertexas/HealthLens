7bdd39f2b1373116612fd01eb37bd800b43a1f52
William Kaiser <wkaisertexas@gmail.com>
2024-07-25 20:39:00 -0500

    format

--- HealthLens/HealthLensApp.swift
+++ HealthLens/HealthLensApp.swift
@@ -1,32 +1,25 @@
-//
-//  HealthLensApp.swift
-//  HealthLens
-//
-//  Created by William Kaiser on 7/1/24.
-//
-
-import SwiftUI
 import SwiftData
+import SwiftUI
 
 @main
 struct HealthLensApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+  var sharedModelContainer: ModelContainer = {
+    let schema = Schema([
+      Item.self
+    ])
+    let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-        .modelContainer(sharedModelContainer)
+    do {
+      return try ModelContainer(for: schema, configurations: [modelConfiguration])
+    } catch {
+      fatalError("Could not create ModelContainer: \(error)")
     }
+  }()
+
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+    .modelContainer(sharedModelContainer)
+  }
 }
