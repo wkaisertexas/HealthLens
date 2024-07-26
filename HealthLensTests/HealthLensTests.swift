b50156bc490c00bba3eaf4b1b48a7ae2babc7936
William Kaiser <wkaisertexas@gmail.com>
2024-07-25 20:36:28 -0500

    tests

--- HealthLensTests/HealthLensTests.swift
+++ HealthLensTests/HealthLensTests.swift
@@ -1,36 +1,48 @@
-//
-//  HealthLensTests.swift
-//  HealthLensTests
-//
-//  Created by William Kaiser on 7/1/24.
-//
-
+import HealthKit
 import XCTest
+
 @testable import HealthLens
 
+/// for testing, use healthkit's testing framework in swift: https://github.com/StanfordBDHG/XCTHealthKit
 final class HealthLensTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+  override func setUpWithError() throws {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+  override func tearDownWithError() throws {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+  func testGeneratePreferredUnitType() async throws {
+    let viewModel = ContentViewModel()
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    // making a sample list of things
+    viewModel.selectedQuantityTypes = [
+      .activeEnergyBurned,
+      .appleMoveTime,
+      .appleStandTime,
+      .bodyMass,
+    ]
+
+    // note: I must be authorized to get the things
+    let prefferedTypes = try! await viewModel.getPreferredUnitType()
+
+    // check to make sure this is approximately the sample length
+    let beforeKeys = viewModel.selectedQuantityTypes.count
+    let afterKeys = prefferedTypes.count
+
+    // checks
+    XCTAssertEqual(beforeKeys, afterKeys, "There should be a preferred type for each key")
+  }
+
+  func testPerformanceExample() throws {
+    // This is an example of a performance test case.
+    self.measure {
+      // Put the code you want to measure the time of here.
+      // we are going to measure the collecting of test information
+
     }
+  }
 
 }
