7b5045d64d05396126f0e3930b9463c089b7c7bb
William Kaiser <wkaisertexas@gmail.com>
2024-07-26 13:18:37 -0500

    dark mode

--- HealthLens/ContentView.swift
+++ HealthLens/ContentView.swift
@@ -1,146 +1,146 @@
-import HealthKit
-import SwiftData
-import SwiftUI
+  import HealthKit
+  import SwiftData
+  import SwiftUI
 
-/// Represents the main content which is present in the application
-struct ContentView: View {
-  @ObservedObject private var contentViewModel = ContentViewModel()
+  /// Represents the main content which is present in the application
+  struct ContentView: View {
+    @ObservedObject private var contentViewModel = ContentViewModel()
 
-  @Environment(\.requestReview) private var requestReview
+    @Environment(\.requestReview) private var requestReview
 
-  var body: some View {
-    NavigationStack{
-      List {
-        Section {
-          VStack {
-            Text("HealthLens").font(.largeTitle).fontWeight(.bold).frame(
-              maxWidth: .infinity, alignment: .leading)
-            Text("Export your health data as a CSV").font(.subheadline).frame(
-              maxWidth: .infinity, alignment: .leading)
-          }
-
-          Link(
-            destination: URL(
-              string: "https://raw.githubusercontent.com/wkaisertexas/HealthLens/main/PRIVACY.md")!
-          ) {
-            HStack {
-              Image(systemName: "lock.shield.fill")
-                .foregroundColor(.blue)
-              Text("Privacy Policy")
+    var body: some View {
+      NavigationStack{
+        List {
+          Section {
+            VStack {
+              Text("HealthLens").font(.largeTitle).fontWeight(.bold).frame(
+                maxWidth: .infinity, alignment: .leading)
+              Text("Export your health data as a CSV").font(.subheadline).frame(
+                maxWidth: .infinity, alignment: .leading)
             }
-          }
 
-          Link(destination: URL(string: "https://github.com/wkaisertexas/healthlens")!) {
-            HStack {
-              Image(systemName: "chevron.left.slash.chevron.right")
-                .foregroundColor(.green)
-              Text("Contribute on GitHub")
-            }
-          }
-
-          Button(action: { requestReview() }) {
-            HStack {
-              Image(systemName: "star.fill")
-                .foregroundColor(.yellow)
-              Text("Leave a Review")
-            }
-          }
-        }
-
-        // Date range selector
-        Section {
-          VStack {
-            Text("Select a date range to export health data").fontWeight(.bold).frame(
-              maxWidth: .infinity, alignment: .leading)
-            DatePicker(
-              "Start Date", selection: $contentViewModel.startDate, in: ...contentViewModel.endDate,
-              displayedComponents: [.date])
-            DatePicker(
-              "End Date", selection: $contentViewModel.endDate,
-              in: contentViewModel.startDate...contentViewModel.currentDate,
-              displayedComponents: [.date])
-          }
-        } header: {
-          Text("Export Range")
-        }
-
-        // Going through each category type
-        ForEach(contentViewModel.categoryGroups, id: \.self) { category in
-          Section(category.name) {
-            ForEach(
-              category.quantities.sorted(by: {
-                contentViewModel.quantityMapping[$1]! > contentViewModel.quantityMapping[$0]!
-              }), id: \.self
-            ) { quant in
-              Button(action: {
-                withAnimation {
-                  contentViewModel.toggleTypeIdentifier(quant)
-                }
-              }) {
-                HStack {
-                    Text(contentViewModel.quantityMapping[quant]!).foregroundStyle(Color.black)
-                  Spacer()
-                  contentViewModel.selectedQuantityTypes.contains(quant)
-                    ? Image(systemName: "checkmark").foregroundColor(.blue) : nil
-                }
+            Link(
+              destination: URL(
+                string: "https://raw.githubusercontent.com/wkaisertexas/HealthLens/main/PRIVACY.md")!
+            ) {
+              HStack {
+                Image(systemName: "lock.shield.fill")
+                  .foregroundColor(.blue)
+                Text("Privacy Policy")
               }
             }
 
-            //                        category.hasBoth ? Spacer() : nil
-            //
-            //                        ForEach(category.categories, id: \.self){ cat in
-            //                            Button(action: {
-            //                                withAnimation{
-            //                                    contentViewModel.toggleTypeIdentifier(cat)
-            //                                }
-            //                            }){
-            //                                HStack{
-            //                                    Text(contentViewModel.categoryMapping[cat]!)
-            //                                    Spacer()
-            //                                    contentViewModel.selectedQuantityTypes.contains(quant) ? Image(systemName: "checkmark").foregroundColor(.blue) : nil
-            //                                }
-            //                            }
-            //                        }
-          }
-        }
-      }
-      .navigationTitle(Text(contentViewModel.selectedQuantityTypes.count > 0 ? "Exporting \(contentViewModel.selectedQuantityTypes.count) item\(contentViewModel.selectedQuantityTypes.count == 1 ? "": "s")" : ""))
-      .navigationBarTitleDisplayMode(.inline)
-      .toolbar {
-        ToolbarItem(placement: .automatic) {
-          ShareLink(
-            item: contentViewModel.shareTarget,
-            preview: SharePreview(
-                "Exporting \(contentViewModel.makeSelectedStringDescription())")
-          ).disabled(contentViewModel.selectedQuantityTypes.count == 0)
-        }
-
-          ToolbarItem(placement: .topBarLeading){
-              Button(action: {
-                withAnimation {
-                  contentViewModel.clearExportQueue()
-                }
-              }) {
-                Image(systemName: "clear")
+            Link(destination: URL(string: "https://github.com/wkaisertexas/healthlens")!) {
+              HStack {
+                Image(systemName: "chevron.left.slash.chevron.right")
+                  .foregroundColor(.green)
+                Text("Contribute on GitHub")
               }
-              .accessibilityHint("Clear the selected HealthKit types")
-              .disabled(contentViewModel.selectedQuantityTypes.count == 0)
+            }
+
+            Button(action: { requestReview() }) {
+              HStack {
+                Image(systemName: "star.fill")
+                  .foregroundColor(.yellow)
+                Text("Leave a Review")
+              }
+            }
           }
+
+          // Date range selector
+          Section {
+            VStack {
+              Text("Select a date range to export health data").fontWeight(.bold).frame(
+                maxWidth: .infinity, alignment: .leading)
+              DatePicker(
+                "Start Date", selection: $contentViewModel.startDate, in: ...contentViewModel.endDate,
+                displayedComponents: [.date])
+              DatePicker(
+                "End Date", selection: $contentViewModel.endDate,
+                in: contentViewModel.startDate...contentViewModel.currentDate,
+                displayedComponents: [.date])
+            }
+          } header: {
+            Text("Export Range")
+          }
+
+          // Going through each category type
+          ForEach(contentViewModel.categoryGroups, id: \.self) { category in
+            Section(category.name) {
+              ForEach(
+                category.quantities.sorted(by: {
+                  contentViewModel.quantityMapping[$1]! > contentViewModel.quantityMapping[$0]!
+                }), id: \.self
+              ) { quant in
+                Button(action: {
+                  withAnimation {
+                    contentViewModel.toggleTypeIdentifier(quant)
+                  }
+                }) {
+                  HStack {
+                    Text(contentViewModel.quantityMapping[quant]!).foregroundStyle(Color.primary)
+                    Spacer()
+                    contentViewModel.selectedQuantityTypes.contains(quant)
+                      ? Image(systemName: "checkmark").foregroundColor(.blue) : nil
+                  }
+                }
+              }
+
+              //                        category.hasBoth ? Spacer() : nil
+              //
+              //                        ForEach(category.categories, id: \.self){ cat in
+              //                            Button(action: {
+              //                                withAnimation{
+              //                                    contentViewModel.toggleTypeIdentifier(cat)
+              //                                }
+              //                            }){
+              //                                HStack{
+              //                                    Text(contentViewModel.categoryMapping[cat]!)
+              //                                    Spacer()
+              //                                    contentViewModel.selectedQuantityTypes.contains(quant) ? Image(systemName: "checkmark").foregroundColor(.blue) : nil
+              //                                }
+              //                            }
+              //                        }
+            }
+          }
+        }
+        .navigationTitle(Text(contentViewModel.selectedQuantityTypes.count > 0 ? "Exporting \(contentViewModel.selectedQuantityTypes.count) item\(contentViewModel.selectedQuantityTypes.count == 1 ? "": "s")" : ""))
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+          ToolbarItem(placement: .automatic) {
+            ShareLink(
+              item: contentViewModel.shareTarget,
+              preview: SharePreview(
+                  "Exporting \(contentViewModel.makeSelectedStringDescription())")
+            ).disabled(contentViewModel.selectedQuantityTypes.count == 0)
+          }
+
+            ToolbarItem(placement: .topBarLeading){
+                Button(action: {
+                  withAnimation {
+                    contentViewModel.clearExportQueue()
+                  }
+                }) {
+                  Image(systemName: "clear")
+                }
+                .accessibilityHint("Clear the selected HealthKit types")
+                .disabled(contentViewModel.selectedQuantityTypes.count == 0)
+            }
+        }
       }
     }
   }
-}
 
-let itemFormatter: DateFormatter = {
-  let formatter = DateFormatter()
+  let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
 
-  formatter.dateStyle = .short
-  formatter.timeStyle = .short
-  formatter.locale = Locale.current
-    
-  return formatter
-}()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .short
+    formatter.locale = Locale.current
+      
+    return formatter
+  }()
 
-#Preview {
-  ContentView()
-}
+  #Preview {
+    ContentView()
+  }
